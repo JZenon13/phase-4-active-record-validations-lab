ac0125c5c4629668a598fa83a467f6a393b3a6e4
jesse zenon <jesse.zenon@gmail.com>
2022-06-28 10:06:46 -0500

    commit tree e29f353fcfc0379f12fdda0d4ce4f087d72dd576

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validates :clickbait, inclusion: {in: %w(Won't believe Secret Top [number] Guess)}
+
 end
